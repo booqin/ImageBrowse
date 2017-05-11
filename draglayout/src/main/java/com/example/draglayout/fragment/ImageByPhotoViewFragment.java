423f7967fc8db1a980a901965da64951bec6387b
vitozhang <developervito@163.com>
2017-05-11 21:10:47 +0800

    1.添加photoView

---
+++ draglayout/src/main/java/com/example/draglayout/fragment/ImageByPhotoViewFragment.java
@@ -0,0 +1,13 @@
+package com.example.draglayout.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * TODO
+ * Created by Boqin on 2017/5/11.
+ * Modified by Boqin
+ *
+ * @Version
+ */
+public class ImageByPhotoViewFragment extends Fragment {
+}
